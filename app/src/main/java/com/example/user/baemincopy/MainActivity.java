0f3ad8e6c5e4d3e8e1373e7749c55708ed056bc9
kikidy12 <kikidy12@naver.com>
2017-08-26 03:28:18 +0900

    배달의민족 초기화면 구현

--- app/src/main/java/com/example/user/baemincopy/MainActivity.java
+++ app/src/main/java/com/example/user/baemincopy/MainActivity.java
@@ -1,40 +1,69 @@
 package com.example.user.baemincopy;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-import com.example.user.baemincopy.adapters.FoodAdapter;
-import com.example.user.baemincopy.utils.GlobalData;
-
-public class MainActivity extends BaseActivity {
-
-    FoodAdapter mAdapter;
-
-    private android.widget.ListView ultraListView;
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        //d adadasdasd
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        FrameLayout testFL = (FrameLayout) findViewById(R.id.testFL);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
         BindViews();
         SetValues();
         SetUpEvents();
-    }
 
-    @Override
-    public void SetUpEvents() {
-    }
-
-    @Override
-    public void SetValues() {
-        mAdapter = new FoodAdapter(mContext, GlobalData.foodDataList);
-        ultraListView.setAdapter(mAdapter);
     }
 
     @Override
     public void BindViews() {
-        this.ultraListView = (ListView) findViewById(R.id.ultraListView);
+
+    }
+
+    @Override
+    public void SetUpEvents() {
+
+    }
+
+    @Override
+    public void SetValues() {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
