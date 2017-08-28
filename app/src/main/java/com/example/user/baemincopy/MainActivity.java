9a0facc9b735e54b8844884ec937ab8e038d6077
kikidy12 <kikidy12@naver.com>
2017-08-28 17:39:23 +0900

    네비게이션 변경

--- app/src/main/java/com/example/user/baemincopy/MainActivity.java
+++ app/src/main/java/com/example/user/baemincopy/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.user.baemincopy;
 
-import android.content.Context;
+import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -11,13 +11,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.ViewGroup;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import net.daum.mf.map.api.MapView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
@@ -30,36 +29,62 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     private FrameLayout testFL;
     private android.widget.ImageView imageView2;
     private android.widget.ImageView imageView;
-    private android.widget.LinearLayout mapview;
     private NavigationView navview;
     private DrawerLayout drawerlayout;
 
-
     private LinearLayout map_view;
+    private TextView lacationTxt;
+    private ActionBarDrawerToggle toggle;
+
+    private Geocoder gCoder;
+    List<Address> addr = null;
+
+    private double lat = 37.494158;
+    private double lng = 126.830503;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        BindViews();
+        SetValues();
+        SetUpEvents();
+    }
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    @Override
+    public void BindViews() {
+        this.drawerlayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        this.navview = (NavigationView) findViewById(R.id.nav_view);
+        this.imageView = (ImageView) findViewById(R.id.imageView);
+        this.imageView2 = (ImageView) findViewById(R.id.imageView2);
+        this.testFL = (FrameLayout) findViewById(R.id.testFL);
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.lacationTxt = (TextView) findViewById(R.id.lacationTxt);
+    }
+
+    @Override
+    public void SetUpEvents() {
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, TestActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void SetValues() {
+        navview.setNavigationItemSelectedListener(this);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        FrameLayout testFL = (FrameLayout) findViewById(R.id.testFL);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+        toggle = new ActionBarDrawerToggle(this, drawerlayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerlayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        TextView lacationTxt = (TextView) findViewById(R.id.lacationTxt);
-        Geocoder gCoder = new Geocoder(mContext, Locale.getDefault());
+        gCoder = new Geocoder(mContext, Locale.getDefault());
 
-        double lat = 37.494158;
-        double lng = 126.830503;
-        List<Address> addr = null;
         try {
             addr = gCoder.getFromLocation(lat, lng, 1);
         } catch (IOException e) {
@@ -67,32 +92,6 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         }
         Address a = addr.get(0);
         lacationTxt.setText(a.getAdminArea()+" "+a.getLocality()+" "+a.getThoroughfare());
-
-        BindViews();
-        SetValues();
-        SetUpEvents();
-
-    }
-
-    @Override
-    public void BindViews() {
-        this.drawerlayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        this.navview = (NavigationView) findViewById(R.id.nav_view);
-        this.mapview = (LinearLayout) findViewById(R.id.map_view);
-        this.imageView = (ImageView) findViewById(R.id.imageView);
-        this.imageView2 = (ImageView) findViewById(R.id.imageView2);
-        this.testFL = (FrameLayout) findViewById(R.id.testFL);
-        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
-    }
-
-    @Override
-    public void SetUpEvents() {
-
-    }
-
-    @Override
-    public void SetValues() {
-
     }
 
     @Override
